711291050ef378579448446fb09d0c4de730a695
YoshiBachu <151955773+YoshiBachu@users.noreply.github.com>
2023-11-25 14:07:55 +0530

    Add files via upload

---
+++ TestVagarant.py
@@ -0,0 +1,14 @@
+class products{
+      int products={leather wallet,umbrella,cigratte,honey};
+       int cost;
+       int gst;
+       int quantity;
+       cost={1100,900,200,100};
+       gst={18%,12%,28%,0%};
+       quantity={1,4,3,2};
+       int max paid gst=cost*quantity;
+       print("max paid gst is");
+       print("total amount is");
+}      
+       
+
